d610bf2e7f6a7905bd56dc4d03dbe7e2b99b146c
kokon322 <chornuy322@gmail.com>
2021-01-12 13:25:13 +0100

    create BookController.java

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,5 +1,37 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.dto.BookAddDto;
+import com.example.demo.entity.dto.BookResponseDto;
+import com.example.demo.services.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+
+    @PostMapping
+    public BookResponseDto save(@RequestBody BookAddDto dto) {
+        return bookService.save(dto);
+    }
+
+    @GetMapping("/getAll")
+    public List<BookResponseDto> getAllBook() {
+        return bookService.readAllBook();
+    }
+
+    @GetMapping("/{id}")
+    public BookResponseDto readById(@PathVariable Long id) {
+        return bookService.readById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteByiId(@PathVariable long id) {
+        bookService.deleteById(id);
+    }
+
 }
