9a82a68c7eea23916969c397252ba53a62e759e6
kokon322 <chornuy322@gmail.com>
2021-01-12 13:11:08 +0100

    Add BookController.java

--- src/main/java/com/example/demo/services/impl/BookService.java
+++ src/main/java/com/example/demo/services/impl/BookService.java
@@ -1,5 +1,7 @@
 package com.example.demo.services.impl;
 
+import com.example.demo.entity.Book;
+import com.example.demo.entity.User;
 import com.example.demo.entity.dto.BookAddDto;
 import com.example.demo.entity.dto.BookResponseDto;
 import com.example.demo.mapper.BookMapper;
@@ -8,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,22 +20,26 @@ public class BookService implements com.example.demo.services.BookService {
 
     @Override
     public BookResponseDto save(BookAddDto dto) {
-
-        return null;
+        Book book = mapper.getBook(dto);
+        book.setAuthor(new User());
+        return mapper.getResponseBookDto(bookRepository.save(book));
     }
 
     @Override
     public BookResponseDto readById(long id) {
-        return null;
+        return mapper.getResponseBookDto(bookRepository.getOne(id));
     }
 
     @Override
     public List<BookResponseDto> readAllBook() {
-        return null;
+        return bookRepository.findAll()
+                .stream()
+                .map(mapper::getResponseBookDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void deleteById(Long id) {
-
+        bookRepository.deleteById(id);
     }
 }
